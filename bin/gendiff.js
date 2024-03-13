66ed5ce7fb981306cac2bf6d4348240288ed8ef6
Yuriy Shulga <jsshu1ga@gmail.com>
2024-03-13 21:19:36 +0300

    Feat support for nested files

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,6 +8,12 @@ program
   .version('1.0.0')
   .option('-f, --format <type>', 'output format')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)));
+  .action((filepath1, filepath2) => {
+    const { format } = program.opts();
+
+    console.log(genDiff(filepath1, filepath2, format));
+  });
+
+//
 
 program.parse();
