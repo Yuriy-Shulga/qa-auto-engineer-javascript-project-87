9a560436971fda9d348a3705041128b8ec83ce11
Yuriy Shulga <jsshu1ga@gmail.com>
2024-03-24 09:49:21 +0300

    Add tests
    Added tests of json output
    Added tests of default values

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -16,6 +16,7 @@ describe('formatters', () => {
 
   const stylishDiff = fs.readFileSync(getFixturePath('stylish.string'), 'utf8');
   const plainDiff = fs.readFileSync(getFixturePath('plain.string'), 'utf8');
+  const jsonDiff = fs.readFileSync(getFixturePath('json.string'), 'utf8');
 
   test('stylish', () => {
     expect(genDiff(json1, json2, 'stylish')).toBe(stylishDiff);
@@ -25,5 +26,34 @@ describe('formatters', () => {
   test('plain', () => {
     expect(genDiff(json1, json2, 'plain')).toBe(plainDiff);
     expect(genDiff(yml1, yml2, 'plain')).toBe(plainDiff);
+  });
+
+  test('json', () => {
+    expect(genDiff(json1, json2, 'json')).toBe(jsonDiff);
+    expect(genDiff(yml1, yml2, 'json')).toBe(jsonDiff);
+  });
+});
+
+describe('default values', () => {
+  const text1 = getFixturePath('file1.txt');
+  const json1 = getFixturePath('file1.json');
+  const json2 = getFixturePath('file2.json');
+
+  const stylishDiff = fs.readFileSync(getFixturePath('stylish.string'), 'utf8');
+
+  test('is an unknown extension', () => {
+    const error = () => genDiff(text1, text1);
+    expect(error).toThrow(Error);
+    expect(error).toThrow('The unknown extension!');
+  });
+
+  test('is an unknown format', () => {
+    const error = () => genDiff(json1, json2, 'abc');
+    expect(error).toThrow(Error);
+    expect(error).toThrow('The unknown format!');
+  });
+
+  test('is a default format', () => {
+    expect(genDiff(json1, json2)).toBe(stylishDiff);
   });
 });
